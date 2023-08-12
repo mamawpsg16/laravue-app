c167499d56e6ed07c8f1ce3b24df14d0f348e18b
Kevin <kevinmensah114@gmail.com>
2023-08-12 16:25:59 -0700

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="counter+=1">{{ counter }}</button>
+        <input type="text" v-model="name">
+        {{ helloWorld }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                counter:0,
+                name:null
+            }
+        },
+        computed:{
+            helloWorld(){
+                return console.log(this.name,'executed');
+            }
+        },
+        methods:{
+           
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
