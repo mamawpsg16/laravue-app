3b8f61381d014e711f22f058a950f72eee8f8f3c
Kevin <kevinmensah114@gmail.com>
2023-08-13 11:12:39 -0700

    update

---
+++ resources/js/components/ListItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <br>
+        <span v-if="id == activeId ">-></span>
+        <!-- <span v-if="isActive">-></span> -->
+        {{ name }}
+</template>
+
+<script>
+    export default {
+        props: ['name','isActive', 'id','activeId'],     
+
+        updated(){
+            console.log('updating',     this.name);
+        }
+    }
+</script>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,30 +1,79 @@
 <template>
     <div>
-        <button @click="counter+=1">{{ counter }}</button>
+        <button @click="counter += 1">{{ counter }}</button>
         <input type="text" v-model="name">
         {{ helloWorld }}
+
+        <list-item  v-for="(people,index ) in updatedPeoples" :name="people.fullName" :id="index" :isActive="activeId == index" :activeId="activeId"></list-item>
+        <br>
+        <button @click="next">Next</button>
+        <!-- <ul style="list-style: none;">
+            <li v-for="people in updatedPeoples">
+                {{ people.fullName }}
+            </li>
+        </ul> -->
     </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                counter:0,
-                name:null
-            }
+import ListItem from '../components/ListItem.vue'
+export default {
+    components: {
+        'list-item' : ListItem
+    },
+    data() {
+        return {
+            counter: 0,
+            name: null,
+            peoples: [
+                {
+                    first_name: 'Maricar',
+                    last_name: 'Lacap',
+                    active: true,
+                },
+                {
+                    first_name: 'Kevin',
+                    last_name: 'Mensah',
+                    active: false,
+                },
+                {
+                    first_name: 'Jerry',
+                    last_name: 'Mensah',
+                    active: false,
+                },
+                {
+                    first_name: 'Marilyn',
+                    last_name: 'Lacap',
+                    active: false,
+                },
+                {
+                    first_name: 'Kristian',
+                    last_name: 'Mensah',
+                    active: false,
+                },
+            ],
+            'activeId':0
+        }
+    },
+    computed: {
+        helloWorld() {
+            return console.log(this.name, 'executed');
         },
-        computed:{
-            helloWorld(){
-                return console.log(this.name,'executed');
-            }
-        },
-        methods:{
-           
+        updatedPeoples() {
+            return this.peoples.map(person => ({
+                ...person,
+                'fullName': `${person.first_name}, ${person.last_name}`
+
+            }))
+        }
+    },
+    methods: {
+        next(){
+            console.log('NEX');
+            this.activeId = (this.activeId + 1) % this.updatedPeoples.length;
         }
     }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
